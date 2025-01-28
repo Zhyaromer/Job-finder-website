cd5efe458ade3b5abacf10750d683374ec95b10f
zhyar <zhyaraland123@gmail.com>
2025-01-28 20:53:19 +0300

    project is done

--- backend/model/FireBase/FirebaseService.js
+++ backend/model/FireBase/FirebaseService.js
@@ -1,15 +1,15 @@
 const { initializeApp } = require("firebase/app");
 const admin = require("firebase-admin");
-const {sendPasswordResetEmail,GoogleAuthProvider,signInWithPopup,signInWithEmailAndPassword} = require("firebase/auth");
-const serviceAccount = require("./job-finder-firebase-adminsdk.json");
+const { sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } = require("firebase/auth");
+const serviceAccount = require("./firebase.js");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLkzo07_CrlMFma3ZUTEznOtr-nouInhE",
-    authDomain: "job-finder-3ff0b.firebaseapp.com",
-    projectId: "job-finder-3ff0b",
-    storageBucket: "job-finder-3ff0b.appspot.com",
-    messagingSenderId: "967835756388",
-    appId: "1:967835756388:web:7c57cacbc7ea7b90f23c71"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 initializeApp(firebaseConfig);
@@ -19,4 +19,4 @@ admin.initializeApp({
 
 const auth = admin.auth();
 
-module.exports = {auth,sendPasswordResetEmail,GoogleAuthProvider,signInWithPopup,signInWithEmailAndPassword};
+module.exports = { auth, sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword };
